3ee7b6b154bb14e473ab334622d00b4323d800bf
momoladebrouill <momoladebrouill@gmail.com>
2024-05-30 12:50:56 +0200

    On met tout dans le theme solaris clair
    Il faudrait ajouter des détails sur la complexité des algos
    et garder la complexité en tant que fil directeur

--- visu.py
+++ visu.py
@@ -1,14 +1,21 @@
 import matplotlib.pyplot as plt
 
-f = open("data", "r")
-data = f.read().split("\n")
-f.close()
+f_euler = open("data_euler", "r")
+data_euler = f_euler.read().split("\n")
+f_euler.close()
+f_rk = open("data_euler_droite", "r")
+data_rk = f_rk.read().split("\n")
+f_rk.close()
 # change theme
 plt.style.use('Solarize_Light2')
-data = [float(i) for i in data if i != ""][:10000]
+#11_500
+#data = [float(i) for i in data_euler if i != ""][:12_000]
+#plt.plot(data)
+data = [float(i) for i in data_rk if i != ""]
 plt.plot(data)
 plt.xlabel("Frame")
 plt.ylabel("Moyenne des normes de vitesse")
 plt.legend()
 # save figure in 1090x720
-plt.savefig("euler.png")
+plt.savefig("euler_gd.png")
+# plt.show()
