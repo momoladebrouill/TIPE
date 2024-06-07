379f6d24a6e39a7e3ce08914854d232b6ff61ed4
momoladebrouill <momoladebrouill@gmail.com>
2024-06-07 18:38:19 +0200

    FINI

--- visu_fps.py
+++ visu_fps.py
@@ -12,14 +12,16 @@ f.close()
 plt.style.use('Solarize_Light2')
 temps1 = [float(k) for k in data1 if k!='']
 temps2 = [float(k) for k in data2 if k!='']
-nb_barres= 150
+weight1 = [2.0 for k in range(len(temps1))]
+weight2 = [2.0 for k in range(len(temps2))]
+nb_barres = 120
 # En vert quand c'est vide
-plt.hist(temps1, bins = nb_barres)
-plt.hist(temps2, bins = nb_barres)
+# plt.hist(temps1, bins = nb_barres, weights = weight1)
+plt.hist(temps2, bins = nb_barres, weights = weight2)
 tmax = max(temps1)
 #plt.axis([0,30,0,50])
 plt.xlabel("Temps (en secondes) ")
-plt.ylabel("Nombre de frames (1 barre = %.2fs)" % round(tmax/nb_barres,2))
+plt.ylabel("FPS")
 plt.legend()
 # save figure in 1090x720
 plt.savefig("FPStvide.png")
